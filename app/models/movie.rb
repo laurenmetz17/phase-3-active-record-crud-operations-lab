8d159b8f60fdddcf42c9230541b6191743f0c479
Lauren Metz <Laurenmetz15@gmail.com>
2023-03-22 13:02:44 -0400

    commit tree 37eed6c75be28ead5fdfcdcf6244f960d0d589f4

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,46 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(name)
+        Movie.create(title: name)
+    end
 
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.all.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > ?" , 2002)
+    end
+
+    def update_with_attributes(attribute)
+        self.update(attribute)
+    end
+
+    def self.update_all_titles(title)
+        Movie.all.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        deleted = Movie.find(id)
+        deleted.destroy
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
